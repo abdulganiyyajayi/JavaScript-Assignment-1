912a90284feb93389f1393b7edfd0c0aebf91ade
Abdulganiyy Ajayi <abdulganiyyajayi@gmail.com>
2016-05-17 18:48:20 -0700

    The search(search_text) functionality is being added (Q 3d)

--- NoteTakingApplication.js
+++ NoteTakingApplication.js
@@ -12,9 +12,20 @@ function NotesApplication(author){
 			return(string);
 		}
 	}
-	
+
 	this.getNote = function(note_id){
 		var strings;
 		strings = this.notes[note_id];
 		return strings;
+	}
+
+	this.search = function(search_text){
+		var strng;
+		strng += 'Showing results for search'+ search_text;
+		for(var i = 0; i < this.notes.length; i++){
+			if(this.notes[i].includes(search_text)){
+				strng += 'Note ID: '+ i +'\n' + this.notes[i] + '\n' + 'By Author' + this.author+ '\n';
+				return(strng);
+			}
+		}
 	}
