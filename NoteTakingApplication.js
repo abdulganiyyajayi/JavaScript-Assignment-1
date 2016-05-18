702fb208a7a6883003372071ced452d817256fff
Abdulganiyy Ajayi <abdulganiyyajayi@gmail.com>
2016-05-17 18:41:55 -0700

    The listNotes() functionality is being added (Q 3b)

--- NoteTakingApplication.js
+++ NoteTakingApplication.js
@@ -4,3 +4,11 @@ function NotesApplication(author){
 	this.create = function(note_content){
 		this.notes.push(note_content);
 	}
+
+	this.listNotes = function(){
+		var string;
+		for(var i =0; i < this.notes.length; i++){
+			string += 'Note ID: '+ i +'\n' + this.notes[i] + '\n' + 'By Author' + this.author+'\n';
+			return(string);
+		}
+	}
