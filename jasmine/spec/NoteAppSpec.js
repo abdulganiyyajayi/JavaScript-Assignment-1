5793484b3222b3d6159c0dc43dc8a80ee56522ac
Abdulganiyy Ajayi <abdulganiyyajayi@gmail.com>
2016-05-19 08:57:48 -0700

    NoteApplication Tests segment added

--- jasmine/spec/NoteAppSpec.js
+++ jasmine/spec/NoteAppSpec.js
@@ -1,49 +1,51 @@
-describe('Attributes created should be valid: ', function() {
+describe('NoteApplication Tests:', function() {
 
-  beforeEach(function() {
-    // var noteApp;
-      noteApp = new NotesApplication('Ajayi'); 
-  });
+    describe('Attributes created should be valid: ', function() {
 
-  it ('notes should be an array', function() {
-    expect(noteApp.notes).toEqual(Array());
-  });
+      beforeEach(function() {
+        // var noteApp;
+          noteApp = new NotesApplication('Ajayi'); 
+      });
 
+      it ('notes should be an array', function() {
+        expect(noteApp.notes).toEqual(Array());
+      });
+
+    });
+
+
+    describe('Each method should return the right data: ', function() {
+      
+
+      beforeEach(function() {
+        // var noteApp;
+          noteApp = new NotesApplication('Ajayi'); 
+      });
+
+      it('create method should only add to the content', function(){
+        expect(noteApp.create()).toBeUndefined();
+      });
+
+      it('listNote should return a string', function() {
+        // noteApp.getNote.();
+        // noteApp.notes = ["eyteafdhae faeygf ieda", "sjflgusfdk sfd"];
+        // spyOn(noteApp, 'getNote');
+        // expect(typeof(noteApp.getNote.calls.argsFor(0)[0])).toBe('string');
+        expect(typeof(noteApp.listNotes())).toBe(typeof("dfsd"));
+      });
+
+      it('getNote should return a string', function() {
+        noteApp.notes[0] = "content1"
+        expect(typeof(noteApp.getNote(0))).toBe(typeof("string"));
+      });
+
+      // it('search method should return a string', function() {
+      //   noteApp.notes[0] = "content1";
+      //   expect(typeof(noteApp.search())).toBe(typeof("string"));
+      // });
+
+      it("edit must return true as the value", function() {
+        expect(noteApp.edit(0,"Welcome")).toBe(true);
+      });
+    });
 });
-
-
-describe('Each method should return the right data: ', function() {
-  
-
-  beforeEach(function() {
-    // var noteApp;
-      noteApp = new NotesApplication('Ajayi'); 
-  });
-
-  it('create method should only add to the content', function(){
-    expect(noteApp.create()).toBeUndefined();
-  });
-
-  it('listNote should return a string', function() {
-    // noteApp.getNote.();
-    // noteApp.notes = ["eyteafdhae faeygf ieda", "sjflgusfdk sfd"];
-    // spyOn(noteApp, 'getNote');
-    // expect(typeof(noteApp.getNote.calls.argsFor(0)[0])).toBe('string');
-    expect(typeof(noteApp.listNotes())).toBe(typeof("dfsd"));
-  });
-
-  it('getNote should return a string', function() {
-    noteApp.notes[0] = "content1"
-    expect(typeof(noteApp.getNote(0))).toBe(typeof("string"));
-  });
-
-  // it('search method should return a string', function() {
-  //   noteApp.notes[0] = "content1";
-  //   expect(typeof(noteApp.search())).toBe(typeof("string"));
-  // });
-
-  it("edit must return true as the value", function() {
-    expect(noteApp.edit(0,"Welcome")).toBe(true);
-  });
-});
-
